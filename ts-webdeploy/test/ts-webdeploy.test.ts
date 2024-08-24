0b0bf8ca64ec3cffbe1da14042078b312f7ff501
Christopher Cichielo <chris.cichielo@gmail.com>
2024-08-24 11:27:34 -0400

    f

---
+++ ts-webdeploy/test/ts-webdeploy.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TsWebdeploy from '../lib/ts-webdeploy-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ts-webdeploy-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TsWebdeploy.TsWebdeployStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
